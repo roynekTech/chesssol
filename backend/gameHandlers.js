a802285c3978a3f76ef8d5c2177d908b44b40be5
NtemKenyor <nkenyor@gmail.com>
2025-04-19 13:41:27 +0100

    yea

--- backend/gameHandlers.js
+++ backend/gameHandlers.js
@@ -3,6 +3,7 @@
 const { getDbConnection } = require('./db');
 const { Chess } = require('chess.js');
 const { v4: uuidv4 } = require('uuid');
+const { spawn } = require('child_process');
 
 
 function assignRandomSide() {
