e3fa67156e434aabc9c839e1403971207055f4d3
NtemKenyor <nkenyor@gmail.com>
2025-04-21 12:34:03 +0100

    on it and more

--- backend/db.js
+++ backend/db.js
@@ -26,19 +26,26 @@
 //   query,
 //   pool
 // };
-
-const mysql = require('mysql2/promise');
 require('dotenv').config();
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0
 });
+
+console.log('ENV:', {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
 
 async function getDbConnection() {
   return await pool.getConnection();
